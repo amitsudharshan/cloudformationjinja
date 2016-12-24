8f75494baf4f289aae3eb8281779aefe68e1275b
Amit Sudharshan <amit@Amits-MacBook-Pro.local>
2016-12-24 10:10:17 -0600

    simple repo to compile cloud formation templates

--- com/amit/testcloudformation/main.py
+++ com/amit/testcloudformation/main.py
@@ -2,6 +2,4 @@ import jinjatemplates as myjinja
 
 if __name__ == '__main__':
     import os
-    f = open("/Users/amit/dev/cftemplates/ec2.json", 'w')
-    f.write(myjinja.render("cloudtemplates/test.json", {}))
-    f.close()
+    print myjinja.render("cloudtemplates/test.json", {})
